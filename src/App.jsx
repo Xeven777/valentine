2f6d6976a487b7b22333b89a7266b0784e4e33ab
Anish <xevenbiswas@gmail.com>
2024-02-05 12:53:02 +0530

    Update HTML and App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import lovesvg from "./assets/All You Need Is Love SVG Cut File.svg";
+import lovesvg2 from "./assets/Love In The Air SVG Cut File.svg";
+
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
@@ -35,20 +38,32 @@ export default function Page() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
+    <div className="flex flex-col items-center pt-52 h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="text-4xl font-bold my-4">Ok yay!!!</div>
+          <div className="text-4xl md:text-6xl font-bold my-4">
+            Ok Yayyyyy!!!
+          </div>
         </>
       ) : (
         <>
           <img
-            className="h-[200px]"
+            src={lovesvg}
+            className="fixed animate-pulse top-10 md:left-24 left-10 md:w-40 w-32"
+          />
+          <img
+            src={lovesvg2}
+            className="fixed bottom-16 animate-pulse md:right-24 right-10 md:w-40 w-32"
+          />
+          <img
+            className="h-[230px] rounded-lg shadow-lg"
             src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="text-4xl my-4">Will you be my Valentine?</h1>
-          <div>
+          <h1 className="text-4xl md:text-6xl my-4 text-center">
+            Will you be my Valentine?
+          </h1>
+          <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
               className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg mr-4`}
               style={{ fontSize: yesButtonSize }}
@@ -58,7 +73,7 @@ export default function Page() {
             </button>
             <button
               onClick={handleNoClick}
-              className=" bg-red-500 hover:bg-red-700 rounded-lg text-white font-bold py-2 px-4"
+              className=" bg-rose-500 hover:bg-rose-700 rounded-lg text-white font-bold py-2 px-4"
             >
               {noCount === 0 ? "No" : getNoButtonText()}
             </button>
@@ -73,8 +88,8 @@ export default function Page() {
 const Footer = () => {
   return (
     <a
-      className="fixed bottom-2 right-2 opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href=""
+      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
+      href="https://github.com/Xeven777/valentine"
       target="__blank"
     >
       Made with{" "}
